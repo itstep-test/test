7c0349fb60d0ac13e6725e3dc9ff776f35498baf
Artem Udovychenko <artemudo90@gmail.com>
2025-01-13 23:12:31 +0200

    [CHC-2] Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
+import Header from "./components/Header/index.jsx";
 
 function App() {
 
-  return (
-    <>
-        <h1>Hello World</h1>
-    </>
-  )
+    return (
+        <>
+            <Header/>
+            <h1>Hello World</h1>
+            <p>Start</p>
+        </>
+    )
 }
 
 export default App
